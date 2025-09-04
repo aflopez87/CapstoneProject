d599a5ce397744c65ab7674e37b87c7ad1e653af
Alexis <aflopez87@gmail.com>
2025-09-04 02:56:40 -0400

    added hours field to user devices table

--- backend/db/meec.sql
+++ backend/db/meec.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS devices;
 DROP TABLE IF EXISTS user_devices;
 DROP TABLE IF EXISTS utilities;
 
-CREATE Table users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     location TEXT NOT NULL,
@@ -25,9 +25,11 @@ CREATE TABLE user_devices(
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     device_id INTEGER NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
-    custom_device TEXT, user_update BOOLEAN DEFAULT TRUE,
+    custom_device TEXT, 
+    user_update BOOLEAN DEFAULT TRUE,
     device_approval BOOLEAN DEFAULT FALSE,
-    usage_algorithm TEXT
+    usage_algorithm TEXT,
+    usage_hours INTEGER
 );
 
 CREATE TABLE utilities (
